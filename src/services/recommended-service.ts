26676a139d6fd9ca689f293d233af4c80f8ed21f
01Sriharsha <harshavasu463@gmail.com>
2024-01-14 09:32:56 +0530

    modified fetch logic of recommend service

--- src/services/recommended-service.ts
+++ src/services/recommended-service.ts
@@ -1,14 +1,40 @@
 import { db } from "@/lib/prisma";
+import { getSelf } from "./auth-service";
+import { User } from "@prisma/client";
 
 export const getRecommended = async () => {
-
   // await new Promise(resolve => setTimeout(resolve , 5000));
 
-  const users = db.user.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
+  let userId;
+
+  try {
+    const self = await getSelf();
+    userId = self?.id;
+  } catch {
+    userId = null;
+  }
+
+  let users: User[] = [];
+
+  if (userId) {
+    //exclude the current logged in user
+    users = await db.user.findMany({
+      where: {
+        NOT: {
+          id: userId,
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  } else {
+    users = await db.user.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }
 
   return users;
 };
