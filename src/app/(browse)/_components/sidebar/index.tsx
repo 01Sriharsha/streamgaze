814dbf9066bbf58e7008dda35f763dee4b1b8462
01Sriharsha <harshavasu463@gmail.com>
2024-01-16 23:10:08 +0530

    refactor: removed a unwanted import

--- src/app/(browse)/_components/sidebar/index.tsx
+++ src/app/(browse)/_components/sidebar/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Wrapper } from "./wrapper";
 import { Toggle, ToggleSkeleton } from "./toggle";
 import { RecommendedSkeleton, Recommeneded } from "./recommended";
