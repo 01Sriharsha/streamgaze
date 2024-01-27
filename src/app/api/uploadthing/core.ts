cfd7fcd859636c5c5076ac87a68594d2e37d08f7
01Sriharsha <harshavasu463@gmail.com>
2024-01-27 22:23:12 +0530

    added logic to upload files to uploadthing and update db

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,30 @@
+import { db } from "@/lib/prisma";
+import { getSelf } from "@/services/auth-service";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  thumbnailUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(async ({ req }) => {
+      const user = await getSelf();
+      if (!user) throw new Error("Unauthorized");
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      const stream = await db.stream.update({
+        where: {
+          userId: metadata.userId,
+        },
+        data: {
+          thumbnailUrl: file.url,
+        },
+      });
+      if (!stream) {
+        throw new Error("Failed to upload image");
+      }
+      return { fileUrl: stream.thumbnailUrl };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
