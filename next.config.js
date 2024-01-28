d0f61908f20a0f0247f4f006253e6932182c2c49
01Sriharsha <harshavasu463@gmail.com>
2024-01-28 23:16:56 +0530

    added uploadthing images hostname

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'img.clerk.com',
-          port: '',
-        },
-      ],
-    },
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+      },
+    ],
+  },
+};
