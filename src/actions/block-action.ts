07ba10e147b50e94486ab6283dcf835b08a03627
01Sriharsha <harshavasu463@gmail.com>
2024-01-31 23:28:09 +0530

    added: removal of blocked user from room

--- src/actions/block-action.ts
+++ src/actions/block-action.ts
@@ -1,16 +1,38 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import { RoomServiceClient } from "livekit-server-sdk";
+
+import { getSelf } from "@/services/auth-service";
 import { blockUser, unBlockUser } from "@/services/block-service";
 
-export const onBlock = async (id: string) => {
-  const blockedUser = await blockUser(id);
-  revalidatePath("/");
+const roomService = new RoomServiceClient(
+  process.env.LIVEKIT_API_URL!,
+  process.env.LIVEKIT_API_KEY!,
+  process.env.LIVEKIT_API_SECRET!
+);
 
-  if (blockedUser) {
-    revalidatePath(`/${blockedUser.blocked.username}`);
-    return blockedUser;
+export const onBlock = async (id: string) => {
+  const self = await getSelf();
+
+  let blockedUser;
+
+  try {
+    blockedUser = await blockUser(id);
+  } catch {
+    //This means that user is a guest
   }
+
+  try {
+    const roomName = self.id;
+    roomService.removeParticipant(roomName, id);
+  } catch {
+    //This means the user is not in the room
+  }
+
+  revalidatePath(`/u/${self.username}/community`);
+
+  return blockedUser;
 };
 
 export const onUnblock = async (id: string) => {
