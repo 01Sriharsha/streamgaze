dd416e2504d551b92e4b721e2f28a293bbb42d4b
01Sriharsha <harshavasu463@gmail.com>
2024-02-02 20:20:59 +0530

    added loading,error and not-found pages for user page

---
+++ src/app/(browse)/[username]/not-found.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+export default function NotFound() {
+  return (
+    <div className="h-full flex flex-col space-y-4 items-center justify-center text-muted-foreground">
+      <h1 className="text-4xl">404</h1>
+      <p>We couldn&apos;t found the user you&apos;re looking for!</p>
+      <Button variant={"primary"} size={"sm"} asChild>
+        <Link href="/" replace>Go Back Home</Link>
+      </Button>
+    </div>
+  );
+}
