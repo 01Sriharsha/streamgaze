aa80579e3f9dca59824696304c26d191d1a88185
01Sriharsha <harshavasu463@gmail.com>
2024-02-03 22:39:05 +0530

    refactor: changed classname

--- src/app/(browse)/_components/navbar/index.tsx
+++ src/app/(browse)/_components/navbar/index.tsx
@@ -5,7 +5,7 @@ import { Actions } from "./actions";
 
 export const Navbar = () => {
   return (
-    <nav className="fixed top-0 w-full h-20 z-49 bg-[#252731] px-3 lg:pg-4 flex justify-between items-center gap-2 shadow-sm">
+    <nav className="fixed top-0 w-full h-20 z-50 bg-[#252731] px-3 lg:pg-4 flex justify-between items-center gap-2 shadow-sm">
       <Logo />
       <Search />
       <Actions />
