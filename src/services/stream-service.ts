a2a8cd5a1ed7a235473123a67b567d601911d210
01Sriharsha <harshavasu463@gmail.com>
2024-02-03 22:39:46 +0530

    feature: added logic to get all streams

--- src/services/stream-service.ts
+++ src/services/stream-service.ts
@@ -1,4 +1,5 @@
 import { db } from "@/lib/prisma";
+import { getSelf } from "./auth-service";
 
 export const getStreamByUserId = async (userId: string) => {
   const stream = await db.stream.findUnique({
@@ -10,4 +11,54 @@ export const getStreamByUserId = async (userId: string) => {
   }
 
   return stream;
+};
+
+export const getStreams = async () => {
+  let streams = [];
+  let userId;
+
+  try {
+    const self = await getSelf();
+    userId = self.id;
+  } catch {
+    userId = null;
+  }
+
+  if (userId) {
+    streams = await db.stream.findMany({
+      where: {
+        user: {
+          NOT: {
+            Blocked: {
+              some: {
+                blockedId: userId,
+              },
+            },
+          },
+        },
+      },
+      orderBy: {
+        isLive: "desc",
+        updatedAt: "desc",
+      },
+      select: {
+        id: true,
+        name: true,
+        user: true,
+        thumbnailUrl: true,
+        isLive: true,
+      },
+    });
+  } else {
+    streams = await db.stream.findMany({
+      select: {
+        id: true,
+        name: true,
+        user: true,
+        thumbnailUrl: true,
+        isLive: true,
+      },
+    });
+  }
+  return streams;
 };
